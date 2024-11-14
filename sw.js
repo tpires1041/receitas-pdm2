588895e4767700c788ea284df009807afa4a0a0e
Tiago Pires <tiagopires2501@outlook.com>
2024-11-14 13:27:22 -0400

    correcoes-professor
    Correções feitas pelo professor, após a avaliação

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 let cacheName = "my-recipes-pwa";
 let filesToCache = [
     "/", "/index", "/css/style.css", "/css/styleR.css", "/js/main.js",
-    "/css/styleRR.css", "receitas-massas", "receitas-doces", "receitas-carnes",
+    "/css/styleRR.css", "/receitas-massas", "receitas-doces", "receitas-carnes",
     "chantilly", "cookie", "lasanha-de-frango", "macarronada", "mingau", "/miojo",
     "panceta", "picadinho-cerveja", "picanha", "images/carnes/panceta/panceta.webp",
     "images/carnes/picadinho-cerveja/picadinho.png", "images/carnes/picanha/picanha.jpg",
     "images/doces/chantilly/chantilly.webp", "images/doces/cookie/cookie.jpg",
     "images/doces/mingau/mingau.jpeg", "images/massas/lasanha/lasanha.jpeg", "images/massas/macarronada/macarronada.webp",
-    "images/massas/miojo/miojo.webp"
+    "images/massas/miojo/miojo.webp", "/manifest.json"
 ];
 
 self.addEventListener("install", (e) => {
